30a4bd95e0646542ef265c62d5439015c5bf7ed2
sriverar03 <srivera.r03@gmail.com>
2022-02-03 21:42:16 -0300

    se agrega carpeta vars en release

---
+++ vars/ejecucion.groovy
@@ -0,0 +1,43 @@
+/*
+	forma de invocación de método call:
+	def ejecucion = load 'script.groovy'
+	ejecucion.call()
+*/
+
+def call(){
+  
+  pipeline {
+
+	agent any
+
+	parameters {
+  		choice choices: ['gradle', 'maven'], description: 'indicar herramienta de construccion', name: 'buildTool'
+  		string(defaultValue: '', name: 'stage', trim: true)
+	}
+
+	stages{
+		stage('Pipeline'){
+			steps{
+				script{				    
+					println "Pipeline"
+
+					if(params.builTools == 'gradle')
+					{
+					    def ejecucion = load 'gradle.groovy'
+	                                    ejecucion.call()
+					}
+					else
+					{
+					    def ejecucion = load 'maven.groovy'
+	                                    ejecucion.call()
+					}
+				}
+			}
+		}		
+		
+	}
+}
+
+}
+
+return this;
