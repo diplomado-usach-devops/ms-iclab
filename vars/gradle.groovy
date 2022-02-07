d97b748dd0d7c21394072dd6d709278ecd065375
sriverar03 <95256304+sriverar03@users.noreply.github.com>
2022-02-06 21:40:59 -0300

    Update gradle.groovy

--- vars/gradle.groovy
+++ vars/gradle.groovy
@@ -90,7 +90,7 @@ def call(String pipeliType){
         {
             stage('Run') { 
                 figlet 'Run'
-                bat "gradle bootRun "           
+                bat "gradle run "           
             }
         }
 
