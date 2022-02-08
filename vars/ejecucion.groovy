2308d0b0e0391b02445a0580d0cc482d6139cde9
sriverar03 <95256304+sriverar03@users.noreply.github.com>
2022-02-07 23:45:21 -0300

    Update ejecucion.groovy

--- vars/ejecucion.groovy
+++ vars/ejecucion.groovy
@@ -36,13 +36,13 @@ def call(){
 	}
 
 	  post {
-	
+			def pipe = BranchName()
 	
 			success {
-				slackSend color: 'good', message: "[Grupo1][Pipeline prueba1][Rama: ${env.GIT_BRANCH}][Stage: ${STAGE}][Resultado OK]"
+				slackSend color: 'good', message: "[Grupo1][Pipeline $pipe][Rama: ${scm.branches[0].name}][Stage: ${STAGE}][Resultado OK]"
 			}
 			failure {
-				slackSend color: 'danger', message: "[Grupo1][Pipeline prueba2][Rama: ${env.GIT_BRANCH}][Stage: ${STAGE}][Resultado No OK]"
+				slackSend color: 'danger', message: "[Grupo1][Pipeline $pipe][Rama: ${scm.branches[0].name}][Stage: ${STAGE}][Resultado No OK]"
 				
 			}
 		
