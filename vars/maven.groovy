fb098e17c5f0be167dbe97cf18b7b591bc96a8a9
sriverar03 <95256304+sriverar03@users.noreply.github.com>
2022-02-07 22:45:34 -0300

    Update maven.groovy

--- vars/maven.groovy
+++ vars/maven.groovy
@@ -115,7 +115,7 @@ def call(String pipeliType){
         {
             stage('nexusUpload') {
                  figlet 'Nexus Upload'
-                bat "curl -v --user admin:123456 --upload-file C:/Users/nmt02/.jenkins/workspace/Laboratorio_Final/pipeline-release/build/libs/pipeline-release-0.0.1.jar http://localhost:8081/repository/lab-m3/com/pipeline-release/pipeline-release/0.0.1/pipeline-release-0.0.1.jar "
+                bat "curl -v --user admin:123456 --upload-file C:/Users/nmt02/.jenkins/workspace/Laboratorio_Final/pipeline-release/build/pipeline-release-0.0.1.jar http://localhost:8081/repository/lab-m3/com/pipeline-release/pipeline-release/0.0.1/pipeline-release-0.0.1.jar "
             } 
         }
         
