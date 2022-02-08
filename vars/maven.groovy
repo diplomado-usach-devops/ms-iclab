4515ab4d2e2ed0d45a2747c957a59e0bc0071796
sriverar03 <95256304+sriverar03@users.noreply.github.com>
2022-02-07 21:05:51 -0300

    Update maven.groovy

--- vars/maven.groovy
+++ vars/maven.groovy
@@ -102,7 +102,7 @@ def call(String pipeliType){
                 url: 'https://github.com/diplomado-usach-devops/ms-iclab.git'
         bat "git pull --rebase" 
         //bat "git checkout -b develop"   
-        bat "git tag -a tag07 -m 'Your tag comment'" 
+        bat "git tag -a tag08 -m 'Your tag comment'" 
         bat "git add ."
         bat "git commit -m 'mensaje de commit'" 
         bat "git push origin develop" 
